98a5bdaeaea0e6f654b0b5a4cee7cf1f0ff535b9
Riseik <emaillard@gaming.tech>
2023-01-26 10:57:20 +0100

    some exercices done

--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -8,6 +8,50 @@ namespace TU_Challenge
 {
     public class MyMathImplementation
     {
-        
+        public static int Add(int a, int b)
+        {
+            return a += b;
+        }
+
+        public static bool IsMajeur(int age) 
+        {
+            return age >= 18;
+        }
+
+        public static bool IsEven(int a) 
+        {
+            return a % 2 == 0;
+        }
+
+        public static bool IsDivisible(int a, int b) 
+        {
+            return a % b == 0;
+        }
+
+        public static bool IsPrimary(int a)
+        {
+            int _nbDiv = 0;
+            for (int i = 1; i <= a; i++) 
+            {
+                if (IsDivisible(a, i)) 
+                {
+                    _nbDiv++;
+                }
+            }
+            return _nbDiv <= 2;
+        }
+
+        public static List<int> GetAllPrimary(int a)
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i < a; i++)
+            {
+                if (IsPrimary(i))
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
     }
 }
